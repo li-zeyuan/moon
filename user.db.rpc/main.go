b8871a7ec2b980a9e7caa41c66c85f73dca7b66f
Ahern <lzyutf8@163.com>
2021-05-23 19:29:07 +0800

    feat: rpc server

--- user.db.rpc/main.go
+++ user.db.rpc/main.go
@@ -1,1 +1,35 @@
-package user_db_rpc
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"user.logic.rpc/pb"
+)
+
+const (
+	port = ":7072"
+)
+
+type server struct {
+	pb.UnimplementedProfileServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.UpsertReq) (*pb.UpsertResp, error) {
+	log.Printf("Received: %v", in.GetUid())
+	return &pb.UpsertResp{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterProfileServiceServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
