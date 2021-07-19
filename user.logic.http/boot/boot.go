89f325017271dd251673a4df940e8ae2034a6474
Ahern <lzyutf8@163.com>
2021-07-19 19:37:23 +0800

    feat: http shutdown

--- user.logic.http/boot/boot.go
+++ user.logic.http/boot/boot.go
@@ -1,17 +1,14 @@
 package boot
 
 import (
-	"net/http"
 	"os"
 
 	"github.com/li-zeyuan/micro/micro.common.api/sequence"
 	"github.com/li-zeyuan/micro/user.logic.http/config"
-	"github.com/li-zeyuan/micro/user.logic.http/router"
 )
 
-func Init(mux *http.ServeMux) {
+func Init() {
 	configPath := os.Getenv(config.ServerConfigPathEvnKey)
 	config.InitConfig(configPath)
-	router.Init(mux)
 	sequence.Init()
 }
--- user.logic.http/main.go
+++ user.logic.http/main.go
@@ -1,19 +1,47 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/li-zeyuan/micro/micro.common.api/logger"
 	"github.com/li-zeyuan/micro/user.logic.http/boot"
 	"github.com/li-zeyuan/micro/user.logic.http/config"
+	"github.com/li-zeyuan/micro/user.logic.http/router"
 )
 
 func main() {
+	boot.Init()
+	srv := http.Server{
+		Addr: ":" + config.Conf.Server.Port,
+	}
+
+	// 创建路由管理器
 	mux := http.NewServeMux()
-	boot.Init(mux)
+	router.Init(mux)
+	srv.Handler = mux
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, os.Kill)
+		<-sigint
+
+		// We received an interrupt signal, shut down.
+		if err := srv.Shutdown(context.Background()); err != nil {
+			// Error from closing listeners, or context timeout:
+			log.Printf("HTTP server Shutdown: %v", err)
+		}
+		close(idleConnsClosed)
+	}()
 
 	logger.DefaultLogger.Infof("server: %s, port: %s", config.Conf.Server.ServiceName, config.Conf.Server.Port)
-	if err := http.ListenAndServe(":"+config.Conf.Server.Port, mux); err != http.ErrServerClosed {
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		logger.DefaultLogger.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
+
+	<-idleConnsClosed
 }
