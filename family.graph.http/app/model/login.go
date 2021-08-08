c6e85d7a5c27b19ab07c09c5262acfe15b0935c9
Ahern <lzyutf8@163.com>
2021-08-08 19:14:58 +0800

    feat: 枚举值

--- family.graph.http/app/model/login.go
+++ family.graph.http/app/model/login.go
@@ -3,7 +3,7 @@ package model
 type FamilyGraphAPICreateReq struct {
 	Phone       string `json:"phone" validate:"eq=11"`
 	Name        string `json:"name" validate:"min=0,max=5"`
-	Gender      int    `json:"gender" validate:"oneof=0 1 2"`
+	Gender      int    `json:"gender" validate:"oneof=1 2"`
 	Birth       int    `json:"birth"`
 	Description string `json:"description"`
 }
