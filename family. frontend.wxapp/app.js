9b79a6a0d1eba1f17283930fac2225430ab78ae1
Ahern <lzyutf8@163.com>
2023-02-05 15:59:39 +0800

    feat: user login

--- family. frontend.wxapp/app.js
+++ family. frontend.wxapp/app.js
@@ -10,6 +10,23 @@ App({
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+            wx.request({
+                method: 'POST',
+                url: 'http://127.0.0.1:7070/api/login/wechat_login',
+                data: {
+                    code: res.code
+                },
+                success: res => {
+                    debugger
+                    if (res.statusCode === 200) {
+                        console.log(res.data.sessionId)// 服务器回包内容
+                    }
+                }
+            })
+        }else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+        }
       }
     })
   },
